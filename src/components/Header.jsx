f00d9d80df687c8e53f5c8e4dee40c681a447e0e
Déborah <deborah.bude@gmail.com>
2023-07-21 11:22:48 +0200

    Initialisation du header et footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/hrnet-logo.png"
+
+export function Header() {
+
+
+    return (
+        <header>
+            <img className="" src={logo} alt="Logo HR Net"/>
+        </header>
+    )
+}
