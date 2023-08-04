26b1c05b1a47f962aa3e66be41be087e9eca1b22
Déborah <deborah.bude@gmail.com>
2023-08-04 16:45:31 +0200

    Mise en place menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import logo from "../assets/hrnet-logo.png"
+import avatar from "../assets/avatar.jpg"
+import {NavLink} from "react-router-dom";
 
 export function Header() {
 
 
     return (
-        <header>
-            <img className="" src={logo} alt="Logo HR Net"/>
+        <header className="entete_page">
+            <div className="entete_page__row">
+                <img className="" src={logo} alt="Logo HR Net"/>
+
+                <nav className="entete_page__nav">
+                    <NavLink className="entete_page__nav-item" to="/">Home</NavLink>
+                    <NavLink className="entete_page__nav-item" to="/employees-list">Employees list</NavLink>
+                </nav>
+            </div>
+            <div className="entete_page__icon_list">
+                <NavLink className="entete_page__icon" to="/"><i className="fa-regular fa-calendar"></i></NavLink>
+                <NavLink className="entete_page__icon" to="/"><i className="fa-regular fa-bell"></i></NavLink>
+                <NavLink className="entete_page__icon" to="/"><i className="fa-regular fa-message"></i></NavLink>
+                <div className="user_profile">
+                    <img src={avatar} alt="Avatar"/>
+                    <p>John Doe</p>
+                    <i className="fa-solid fa-chevron-down"></i>
+                </div>
+            </div>
         </header>
     )
 }
