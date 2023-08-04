0ddd27212c5fb0d15046fc5f9240ec5a446b9d89
Déborah <deborah.bude@gmail.com>
2023-08-04 16:45:53 +0200

    Mise en place navigation et route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
+import {Route, Routes} from "react-router-dom";
+import {Home} from "./page/Home";
+import {EmployeesList} from "./page/Employee-list";
+import {Provider} from "react-redux";
+import {store} from "./redux";
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <Footer/>
-    </div>
+      <Provider store={store}>
+          <Header/>
+          <section className="corps_page">
+              <Routes>
+                  <Route path="/" element={<Home/>}></Route>
+                  <Route path="/employees-list" element={<EmployeesList/>}></Route>
+              </Routes>
+          </section>
+          <Footer/>
+      </Provider>
   );
 }
 
