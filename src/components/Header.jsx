87bdbfeb337f4f939c7144745b11e7d9bbb9e1fc
Déborah <deborah.bude@gmail.com>
2023-09-08 22:37:49 +0200

    Modify header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "../assets/hrnet-logo.png"
-import avatar from "../assets/avatar.jpg"
 import {NavLink} from "react-router-dom";
 
 export function Header() {
@@ -15,16 +14,6 @@ export function Header() {
                     <NavLink className="entete_page__nav-item" to="/">Home</NavLink>
                     <NavLink className="entete_page__nav-item" to="/employees-list">Employees list</NavLink>
                 </nav>
-            </div>
-            <div className="entete_page__icon_list">
-                <NavLink className="entete_page__icon" to="/"><i className="fa-regular fa-calendar"></i></NavLink>
-                <NavLink className="entete_page__icon" to="/"><i className="fa-regular fa-bell"></i></NavLink>
-                <NavLink className="entete_page__icon" to="/"><i className="fa-regular fa-message"></i></NavLink>
-                <div className="user_profile">
-                    <img src={avatar} alt="Avatar"/>
-                    <p>John Doe</p>
-                    <i className="fa-solid fa-chevron-down"></i>
-                </div>
             </div>
         </header>
     )
